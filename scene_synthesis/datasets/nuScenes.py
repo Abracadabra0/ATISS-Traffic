e537f6a595eeab85fbcff2355815a3b4e91bccd4
Yifan Lin <yflin001217@gmail.com>
2022-05-01 18:05:51 +0800

    parse dataset

---
+++ scene_synthesis/datasets/nuScenes.py
@@ -0,0 +1,122 @@
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+import os
+from pyquaternion import Quaternion
+from nuscenes.map_expansion.map_api import NuScenesMap
+from nuscenes.nuscenes import NuScenes
+from nuscenes.utils.geometry_utils import BoxVisibility
+from utils import get_homogeneous_matrix, cartesian_to_polar
+
+
+class NuScenesDataset(Dataset):
+    layer_names = ['drivable_area',
+                   'road_segment',
+                   'road_block',
+                   'lane',
+                   'ped_crossing',
+                   'walkway',
+                   'stop_line',
+                   'carpark_area',
+                   'road_divider',
+                   'lane_divider',
+                   'traffic_light']
+    Q = 0
+    PEDESTRIAN = 1
+    BICYCLIST = 2
+    VEHICLE = 3
+    END = 4
+    category_mapping = {'human.pedestrian.adult': PEDESTRIAN,
+                        'human.pedestrian.child': PEDESTRIAN,
+                        'human.pedestrian.wheelchair': PEDESTRIAN,
+                        'human.pedestrian.stroller': PEDESTRIAN,
+                        'human.pedestrian.personal_mobility': PEDESTRIAN,
+                        'human.pedestrian.police_officer': PEDESTRIAN,
+                        'human.pedestrian.construction_worker': PEDESTRIAN,
+                        'vehicle.car': VEHICLE,
+                        'vehicle.motorcycle': BICYCLIST,
+                        'vehicle.bicycle': BICYCLIST,
+                        'vehicle.bus.bendy': VEHICLE,
+                        'vehicle.bus.rigid': VEHICLE,
+                        'vehicle.truck': VEHICLE,
+                        'vehicle.construction': VEHICLE,
+                        'vehicle.emergency.ambulance': VEHICLE,
+                        'vehicle.emergency.police': VEHICLE,
+                        'vehicle.trailer': VEHICLE}
+
+    @classmethod
+    def preprocess(cls, dataroot: str,
+                   version: str,
+                   output_path: str,
+                   axes_limit: int = 40):
+        nusc = NuScenes(version=version, dataroot=dataroot, verbose=False)
+        os.makedirs(output_path, exist_ok=True)
+        os.chdir(output_path)
+        # cache all nusc maps
+        maps_cache = {}
+        for sample in nusc.sample:
+            # get data from that sample
+            sample_data = nusc.get('sample_data', sample['data']['LIDAR_TOP'])
+            scene = nusc.get('scene', sample['scene_token'])
+            log = nusc.get('log', scene['log_token'])
+            map_name = log['location']
+            pose = nusc.get('ego_pose', sample_data['ego_pose_token'])
+            ego_to_world = get_homogeneous_matrix(np.zeros(3), Quaternion(pose['rotation']).rotation_matrix)
+
+            # create directory
+            os.makedirs(sample_data['token'], exist_ok=True)
+            os.chdir(sample_data['token'])
+
+            # get annotated map
+            try:
+                nusc_map = maps_cache[map_name]
+            except KeyError:
+                nusc_map = NuScenesMap(dataroot=dataroot, map_name=map_name)
+                maps_cache[map_name] = nusc_map
+            patch_box = (pose['translation'][0], pose['translation'][1], axes_limit * 2, axes_limit * 2)
+            patch_angle = 0  # Default orientation where North is up
+            map_mask = nusc_map.get_map_mask(patch_box, patch_angle, cls.layer_names, canvas_size=None)
+            map_mask = np.flip(map_mask, 1)
+            # convert to torch.tensor and save it
+            map_mask = torch.tensor(map_mask.copy(), dtype=torch.float32)
+            torch.save(map_mask, 'map')
+
+            # retrieve all objects that fall inside the boundaries
+            _, boxes, _ = nusc.get_sample_data(sample['data']['LIDAR_TOP'], box_vis_level=BoxVisibility.ANY,
+                                               use_flat_vehicle_coordinates=True)
+            boxes = filter(lambda x: -axes_limit < x.center[0] < axes_limit and -axes_limit < x.center[1] < axes_limit,
+                           boxes)
+            # filter out relevant categories
+            boxes = filter(lambda x: x.name in cls.category_mapping, boxes)
+            # parse data
+            category = []
+            location = []
+            bbox = []
+            velocity = []
+            for box in boxes:
+                box_to_ego = get_homogeneous_matrix(box.center, box.rotation_matrix)
+                box_to_world = np.dot(ego_to_world, box_to_ego)
+                # calculates vehicle heading direction
+                _, heading = cartesian_to_polar(box_to_world[:2, 0])
+                # calculates velocity by differentiate
+                v = nusc.box_velocity(box.token)[:2]
+                # velocity could be nan. If so, drop it
+                if True in np.isnan(v):
+                    continue
+                category.append(cls.category_mapping[box.name])
+                location.append(box_to_world[:2, -1])
+                bbox.append((box.wlh[0], box.wlh[1], heading))
+                velocity.append(cartesian_to_polar(v))
+            # convert to tensor and save
+            torch.save(torch.tensor(category, dtype=torch.int64), 'category')
+            torch.save(torch.tensor(location, dtype=torch.float32), 'location')
+            torch.save(torch.tensor(bbox, dtype=torch.float32), 'bbox')
+            torch.save(torch.tensor(velocity, dtype=torch.float32), 'velocity')
+
+            os.chdir('..')
+
+
+if __name__ == '__main__':
+    NuScenesDataset.preprocess('/media/yifanlin/My Passport/data/nuScene',
+                               'v1.0-mini',
+                               '/media/yifanlin/My Passport/data/nuScene-processed')
--- test.py
+++ test.py
@@ -1,15 +1,10 @@
-import math
 import numpy as np
 from matplotlib import pyplot as plt
-from PIL import Image
 from pyquaternion import Quaternion
 from nuscenes.map_expansion.map_api import NuScenesMap
 from nuscenes.nuscenes import NuScenes
+from nuscenes.utils.geometry_utils import BoxVisibility
 
-dataroot = '/media/yifanlin/My Passport/data/nuScene'
-nusc = NuScenes(version='v1.0-mini', dataroot=dataroot, verbose=True)
-sample = nusc.sample[20]
-axes_limit = 80
 
 def crop_image(image: np.array,
                x_px: int,
@@ -22,6 +17,19 @@ def crop_image(image: np.array,
     cropped_image = image[y_min:y_max, x_min:x_max]
     return cropped_image
 
+
+def get_homogeneous_matrix(translation: np.array,
+                           rotation: np.array) -> np.array:
+    homogeneous_matrix = np.eye(4)
+    homogeneous_matrix[:3, :3] = rotation
+    homogeneous_matrix[:3, 3] = translation
+    return homogeneous_matrix
+
+
+dataroot = '/media/yifanlin/My Passport/data/nuScene'
+nusc = NuScenes(version='v1.0-mini', dataroot=dataroot, verbose=False)
+sample = nusc.sample[8]
+axes_limit = 40
 # Get data.
 sample_data = nusc.get('sample_data', sample['data']['LIDAR_TOP'])
 scene = nusc.get('scene', sample['scene_token'])
@@ -42,10 +50,28 @@ nusc.render_sample_data(sample['data']['LIDAR_TOP'], axes_limit=40)
 plt.show()
 
 nusc_map = NuScenesMap(dataroot=dataroot, map_name=map_name)
-patch_box = (pose['translation'][0], pose['translation'][1], axes_limit * 2, axes_limit * 2)
-patch_angle = 0  # Default orientation where North is up
-layer_names = None
-canvas_size = None
-map_mask = nusc_map.get_map_mask(patch_box, patch_angle, layer_names, canvas_size)
-plt.imshow(map_mask[0])
+# patch_box = (pose['translation'][0], pose['translation'][1], axes_limit * 2, axes_limit * 2)
+# patch_angle = 0  # Default orientation where North is up
+# layer_names = nusc_map.non_geometric_layers
+# map_mask = nusc_map.get_map_mask(patch_box, patch_angle, layer_names, canvas_size=None)
+# map_mask = np.flip(map_mask, 1)
+# for layer, mask in zip(layer_names, map_mask):
+#     plt.imshow(mask)
+#     plt.title(layer)
+#     plt.show()
+
+ego_to_world = get_homogeneous_matrix(np.zeros(3), Quaternion(pose['rotation']).rotation_matrix)
+_, boxes, _ = nusc.get_sample_data(sample['data']['LIDAR_TOP'], box_vis_level=BoxVisibility.ANY,
+                                   use_flat_vehicle_coordinates=True)
+_, ax = plt.subplots(1, 1, figsize=(9, 9))
+ax.imshow(cropped, extent=[-axes_limit, axes_limit, -axes_limit, axes_limit])
+for box in boxes:
+    box_to_ego = get_homogeneous_matrix(box.center, box.rotation_matrix)
+    box_to_world = np.dot(ego_to_world, box_to_ego)
+    x, y = box_to_world[:2, 3]
+    ax.plot(x, y, 'x', color='red')
+    dx, dy, _ = nusc.box_velocity(box.token)
+    ax.arrow(x, y, dx * 10, dy * 10, color='blue', width=0.05)
+ax.set_xlim(-axes_limit, axes_limit)
+ax.set_ylim(-axes_limit, axes_limit)
 plt.show()

35702824a9faf276037012374579175ddef218b1
Yifan Lin <yflin001217@gmail.com>
2022-07-02 11:11:47 +0800

    visualize probability

--- visualize.py
+++ visualize.py
@@ -12,10 +12,11 @@ np.random.seed(0)
 torch.manual_seed(0)
 dataset = NuScenesDataset("/media/yifanlin/My Passport/data/nuScene-processed", train=True)
 axes_limit = 40
+_, ax = plt.subplots()
 cat2color = {1: 'red', 2: 'blue', 3: 'green'}
 feature_extractor = ResNet18(4, 512)
 model = AutoregressiveTransformer(feature_extractor)
-model.load_state_dict(torch.load('./ckpts/07-01-04:15:14'))
+model.load_state_dict(torch.load('./ckpts/07-01-05:01:13'))
 
 data = dataset[11]
 input_data, length, _ = collate_train([data])
@@ -23,12 +24,12 @@ for k in data:
     data[k] = input_data[k].squeeze(0).numpy()
 
 map_layers = data['map'].sum(axis=0)
-plt.imshow(map_layers, extent=[-axes_limit, axes_limit, -axes_limit, axes_limit])
+ax.imshow(map_layers, extent=[-axes_limit, axes_limit, -axes_limit, axes_limit])
 for i in range(length.item()):
     if data['category'][i] != 0:
         color = cat2color[data['category'][i]]
         loc = data['location'][i]
-        plt.plot(loc[0], loc[1], 'x', color=color)
+        ax.plot(loc[0], loc[1], 'x', color=color)
         w, l, theta = data['bbox'][i]
         corners = np.array([[l / 2, w / 2],
                             [-l / 2, w / 2],
@@ -38,12 +39,12 @@ for i in range(length.item()):
         rotation = np.array([[np.cos(theta), np.sin(theta)],
                              [-np.sin(theta), np.cos(theta)]])
         corners = np.dot(corners, rotation) + loc
-        plt.plot(corners[:, 0], corners[:, 1], color=color, linewidth=2)
+        ax.plot(corners[:, 0], corners[:, 1], color=color, linewidth=2)
         speed, omega = data['velocity'][i]
         rotation = np.array([[np.cos(omega), np.sin(omega)],
                              [-np.sin(omega), np.cos(omega)]])
         velocity = np.dot(np.array([speed, 0]), rotation)
-        plt.arrow(loc[0], loc[1], velocity[0] * 5, velocity[1] * 5, color=color, width=0.05)
+        ax.arrow(loc[0], loc[1], velocity[0] * 5, velocity[1] * 5, color=color, width=0.05)
 
 preds, probs = model.generate(input_data, length)
 for k in preds:
@@ -63,7 +64,7 @@ category = preds['category']
 if category != 0:
     color = cat2color[category]
     loc = preds['location']
-    plt.plot(loc[0], loc[1], 'x', color=color)
+    ax.plot(loc[0], loc[1], 'x', color=color)
     w, l = preds['bbox']['wl']
     theta = preds['bbox']['theta']
     corners = np.array([[l / 2, w / 2],
@@ -74,13 +75,13 @@ if category != 0:
     rotation = np.array([[np.cos(theta), np.sin(theta)],
                          [-np.sin(theta), np.cos(theta)]])
     corners = np.dot(corners, rotation) + loc
-    plt.plot(corners[:, 0], corners[:, 1], color=color, linewidth=2)
+    ax.plot(corners[:, 0], corners[:, 1], color=color, linewidth=2)
     speed = preds['velocity']['s'] * preds['velocity']['moving']
     omega = preds['velocity']['omega']
     rotation = np.array([[np.cos(omega), np.sin(omega)],
                          [-np.sin(omega), np.cos(omega)]])
     velocity = np.dot(np.array([speed, 0]), rotation)
-    plt.arrow(loc[0], loc[1], velocity[0] * 5, velocity[1] * 5, color=color, width=0.05)
+    ax.arrow(loc[0], loc[1], velocity[0] * 5, velocity[1] * 5, color=color, width=0.05)
 data['category'] = np.concatenate([data['category'], np.array([preds['category']])], axis=0)
 data['location'] = np.concatenate([data['location'], preds['location'].reshape(1, -1)], axis=0)
 data['bbox'] = np.concatenate([data['bbox'], np.concatenate([preds['bbox']['wl'], np.array([preds['bbox']['theta']])], axis=0).reshape(1, -1)], axis=0)
@@ -91,4 +92,10 @@ for k in ['location', 'bbox', 'velocity', 'map']:
     input_data[k] = torch.tensor(data[k], dtype=torch.float)
 input_data, length, _ = collate_train([input_data])
 
-grid = np.meshgrid(np.linspace(-axes_limit, axes_limit, 400), np.linspace(-axes_limit, axes_limit, 400))
+_, ax_prob = plt.subplots()
+grid = np.meshgrid(np.linspace(-axes_limit, axes_limit, 400), np.linspace(axes_limit, -axes_limit, 400))
+grid = np.stack(grid, axis=-1)
+prob_map = probs['location'].log_prob(torch.tensor(grid / 40))
+ax_prob.imshow(prob_map, extent=[-axes_limit, axes_limit, -axes_limit, axes_limit])
+mean = probs['location'].component_distribution.mean.squeeze().numpy() * 40
+ax_prob.scatter(mean[:, 0], mean[:, 1])

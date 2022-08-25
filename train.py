475ebf8476bd059d4a94f03bc9b9e7b6d25911a8
Yifan Lin <yflin001217@gmail.com>
2022-08-25 22:19:33 +0800

    score model

---
+++ train.py
@@ -0,0 +1,41 @@
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from datasets import NuScenesDataset, ScoreModelProcessor, collate_fn
+from networks.ScoreBasedGenerator import Generator
+import numpy as np
+import time
+
+if __name__ == '__main__':
+    device = torch.device(0)
+    np.random.seed(0)
+    torch.manual_seed(0)
+    timestamp = time.strftime('%m-%d-%H:%M:%S')
+    writer = SummaryWriter(log_dir=f'./log/{timestamp}')
+    os.makedirs('./ckpts', exist_ok=True)
+    dataset = NuScenesDataset("../../data/nuScene-processed/train")
+    dataloader = DataLoader(dataset, batch_size=16, shuffle=True, num_workers=4, collate_fn=collate_fn)
+    processor = ScoreModelProcessor(device).train()
+    model = Generator()
+    model.to(device)
+    n_epochs = 400
+    iters = 0
+
+    for epoch in range(n_epochs):
+        print(f'----------------Epoch {epoch}----------------')
+        for batch in dataloader:
+            x, map_layers = processor(batch)
+            loss = model(x, map_layers)
+            print(iters, loss)
+            writer.add_scalar('loss', loss, iters)
+            iters += 1
+
+        if (epoch + 1) % 100 == 0:
+            model.cpu()
+            torch.save(model.state_dict(), os.path.join('./ckpts', timestamp + f'-{epoch}'))
+            model.to(device)
+
+    model.cpu()
+    torch.save(model.state_dict(), os.path.join('./ckpts', timestamp))

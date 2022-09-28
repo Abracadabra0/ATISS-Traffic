9ca1f2c0a2ab3ba2172ee0a5f876333ec5b31113
YifanLin <yflin001217@gmail.com>
2022-09-28 02:21:32 -0500

    betteer quality blurring

--- networks/diffusion_models.py
+++ networks/diffusion_models.py
@@ -46,6 +46,7 @@ class DiffusionBasedModel(nn.Module):
     def blur(img, factor):
         size = img.shape[-1]
         new_size = int(size / factor)
+        blurred = nn.AdaptiveAvgPool2d(new_size)(img)
         blurred = functional.resize(img, [new_size, new_size])
         blurred = functional.gaussian_blur(blurred, [3, 3], [0.5, 0.5])
         blurred = functional.resize(blurred, [size, size])
